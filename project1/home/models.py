1c787e8ba542d8a7778374ef1b5e264231ac72c3
codewithazhar456 <skazharuddinking637@gmail.com>
2025-02-21 00:11:07 +0530

    relationship in models

--- project1/home/models.py
+++ project1/home/models.py
@@ -9,3 +9,21 @@ class student(models.Model):
     date_of_birth=models.models.DateField
     created_at=models.DateTimeField(auto_created=True)
     updated_at=models.DateTimeField(auto_now_add=True)
+
+
+class author(models.Model):
+    author_name=models.CharField(max_length=140)
+
+class book(models.Model):
+    author=models.OneToOneField(author,on_delete=models.CASCADE)
+    book=models.CharField(max_length=140)
+
+
+
+class Brand(models.Model):
+    brand=models.CharField(max_length=123)
+
+class products(models.Model):
+    brand=models.ForeignKey(Brand,on_delete=models.RESTRICT)
+    products=models.CharField(123)
+
